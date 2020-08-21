e08a99c1b716c6d568ccde35545d5d39e77884c5
mohit <mohit.dhiman.sarkaghat@gmail.com>
2020-08-21 12:12:35 +0530

    Creating a prev and next components

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -25,6 +25,17 @@
 // </template> -->
 
 <template>
+  <!-- <article>
+    <h1>{{ article.title }}</h1>
+    <p>{{ article.description }}</p>
+    <img :src="article.img" :alt="article.alt" />
+    <p>Article last updated: {{ formatDate(article.updatedAt) }}</p>
+
+    <nuxt-content :document="article" />
+
+    <author :author="article.author" />
+  </article> -->
+
   <article>
     <h1>{{ article.title }}</h1>
     <p>{{ article.description }}</p>
@@ -34,15 +45,33 @@
     <nuxt-content :document="article" />
 
     <author :author="article.author" />
+
+    <prev-next :prev="prev" :next="next" />
   </article>
 </template>
+
 <script>
   export default {
-    async asyncData({ $content, params }) {
-      const article = await $content('articles', params.slug).fetch()
+    // async asyncData({ $content, params }) {
+    //   const article = await $content('articles', params.slug).fetch()
 
-      return { article }
-    },
+    //   return { article }
+    // },
+    async asyncData({ $content, params }) {
+    const article = await $content('articles', params.slug).fetch()
+
+    const [prev, next] = await $content('articles')
+      .only(['title', 'slug'])
+      .sortBy('createdAt', 'asc')
+      .surround(params.slug)
+      .fetch()
+
+    return {
+      article,
+      prev,
+      next
+    }
+  },
     methods: {
     formatDate(date) {
       const options = { year: 'numeric', month: 'long', day: 'numeric' }
